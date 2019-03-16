a84965a9181ef8a7b313de1997bfef5c6a1f9363
HiImSenya <48597939+HiImSenya@users.noreply.github.com>
2019-03-16 14:19:00 +0300

    Update Head.cs

--- Project/ConsoleApp6/Head.cs
+++ Project/ConsoleApp6/Head.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp6
             Customer customer = new Customer();
 
             Console.WriteLine("Добрый день , прошу вас пройти простую процедуру регистрации");
-
+            Console.WriteLine("HelloNewFcBrunch");
             Console.WriteLine("Введите Логин");
             customer.Login = Console.ReadLine();
             Console.WriteLine("Введите Пароль");
